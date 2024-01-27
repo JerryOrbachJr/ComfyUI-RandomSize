117eb0b8801b221dacead30d064f553a1f7b4fca
JO Jr. <jerryorbach@proton.me>
2024-01-27 12:15:03 -0500

    classmethod

--- random_size.py
+++ random_size.py
@@ -8,10 +8,9 @@ def read_config(item):
 
 class RandomSize:
     CATEGORY = "utils"
-    def INPUT_TYPES(cls):
-        return {
-            "required": {},
-        }
+    @classmethod    
+    def INPUT_TYPES(s):
+        return { "required":{} }
     RETURN_TYPES = ("INT","INT")
     RETURN_NAMES = ("width","height")
     FUNCTION = "func"
