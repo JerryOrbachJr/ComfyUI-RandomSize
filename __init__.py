33170f38d0c0d86bb957ac8a2e8ec1a23edccf95
JO Jr. <jerryorbach@proton.me>
2024-01-27 12:07:34 -0500

    fix install bug

--- __init__.py
+++ __init__.py
@@ -7,11 +7,12 @@
 """
 
 from .random_size import RandomSize
-LIVE_NODE_CLASS_MAPPINGS = {
+NODE_CLASS_MAPPINGS = {
     "JOJR_RandomSize": RandomSize,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "JOJR_RandomSize": "Random Size",
 }
+
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
