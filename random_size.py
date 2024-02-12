da66fe76067fa24479f8300f3068428b10fe7b0b
JerryOrbachJr <jerryorbach@proton.me>
2024-02-11 23:14:11 -0500

    Move to preset files

--- random_size.py
+++ random_size.py
@@ -1,23 +1,36 @@
 import yaml, os, random
-module_root_directory = os.path.dirname(os.path.realpath(__file__))
-config_file = os.path.join(module_root_directory,'sizes.yaml')
-# TODO: make inputs?
-def read_config(item):
-    with open(config_file, 'r') as file:
-        return yaml.safe_load(file)[item]
+sizes_dir = os.path.join(os.path.dirname(__file__), "sizes")
+sizes_dir = os.path.abspath(sizes_dir)
+
+def read_config(preset_file):
+    if preset_file == "Preset":
+        preset_file_path = os.path.join(sizes_dir,"SD1.5.yaml")
+    else:
+        preset_file_path = os.path.join(sizes_dir,preset_file)
+    with open( preset_file_path, 'r') as file:
+        return yaml.safe_load(file)['sizes']
+
+def load_presets():
+    preset_list = [file for file in os.listdir(sizes_dir) if file.endswith(".yaml")]
+    return preset_list
 
 class RandomSize:
     CATEGORY = "utils"
     @classmethod
     def INPUT_TYPES(cls):
+        preset = ["Preset"]  # 20
+        preset += load_presets()
         return {
-            "required": { "seed": ("INT", {"default": 0, "min": 0, "max": 0xffffffffffffffff}) },
+            "required": { 
+                "seed": ("INT", {"default": 0, "min": 0, "max": 0xffffffffffffffff}),
+                "preset": (preset,)
+            },
         }
     RETURN_TYPES = ("INT","INT")
     RETURN_NAMES = ("width","height")
     FUNCTION = "func"
-    def func(self:str,seed):
-        sizes = read_config('sizes')
+    def func(self:str,seed, preset):
+        sizes = read_config(preset)
         rand_obj = random.Random(seed)
         size = rand_obj.choice(sizes)
         x, y = [int(v) for v in size.split('x')]
